90ec2aa44b9945133b5d5f266e1b0af9117995a7
Kristof Van Espen <kristof@creativebrain.be>
2014-04-25 19:00:34 +0200

    Database class

---
+++ classes/Db.class.php
@@ -0,0 +1,14 @@
+<?php 
+	class Db
+	{
+		private $m_sHost = "localhost";
+		private $m_sUser = "root";
+		private $m_sPassword = "";
+		private $m_sDatabase = "opentable";
+
+		public function __construct()
+		{
+			$this->conn = new mysqli($this->m_sHost, $this->m_sUser, $this->m_sPassword, $this->m_sDatabase);
+		}
+	}
+ ?>
