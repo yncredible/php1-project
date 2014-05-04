96f92ef7f4d53d8e687c9dda9279b12429091767
Kristof Van Espen <kristof@creativebrain.be>
2014-05-04 22:53:12 +0200

    Added page redirect when not logged in

--- index.php
+++ index.php
@@ -2,30 +2,41 @@
 	
 	session_start();
 
-	//delete previous sessions
-	if(isset($_SESSION['name']))
-	{session_unset($_SESSION['name']);}
+	if(isset($_SESSION['ownerIdentity']))
+	{
+		//delete previous sessions
+		if(isset($_SESSION['name']))
+		{session_unset($_SESSION['name']);}
 
-	if(isset($_SESSION['email']))
-	{session_unset($_SESSION['email']);}
+		if(isset($_SESSION['email']))
+		{session_unset($_SESSION['email']);}
 
-	if(isset($_SESSION['password']))
-	{session_unset($_SESSION['password']);}
+		if(isset($_SESSION['password']))
+		{session_unset($_SESSION['password']);}
 
-	
-	$ownerIdentity = $_SESSION['ownerIdentity'];
-	//user $ownerIdentity to recognize what user you're targetting;
-	//login page has to fall on this session as well
-	echo $ownerIdentity;
+		
+		$ownerIdentity = $_SESSION['ownerIdentity'];
+		//user $ownerIdentity to recognize what user you're targetting;
+		//login page has to fall on this session as well
+		echo $ownerIdentity;
+	}
+	else
+	{
+		header('location: login.php');
+	}
 	
 
  ?><!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+
+
+	<body>
+		
+		<a href="logout.php">Log Out</a>
+
+	</body>
 </html>
