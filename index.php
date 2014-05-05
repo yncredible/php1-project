84ec3582cc424ea4413d485be763ceb1cd48e49f
kimberlygs <kimberlygs@hotmail.be>
2014-05-05 18:21:29 +0200

    aanpassing index.php,uitlussen restaurants

--- index.php
+++ index.php
@@ -2,41 +2,57 @@
 	
 	session_start();
 
-	if(isset($_SESSION['ownerIdentity']))
-	{
-		//delete previous sessions
-		if(isset($_SESSION['name']))
-		{session_unset($_SESSION['name']);}
+	include_once('classes/Restaurant.class.php');
+	include_once('classes/Restaurateur.class.php');
 
-		if(isset($_SESSION['email']))
-		{session_unset($_SESSION['email']);}
+	//delete previous sessions
+	if(isset($_SESSION['name']))
+	{session_unset($_SESSION['name']);}
 
-		if(isset($_SESSION['password']))
-		{session_unset($_SESSION['password']);}
+	if(isset($_SESSION['email']))
+	{session_unset($_SESSION['email']);}
 
-		
-		$ownerIdentity = $_SESSION['ownerIdentity'];
-		//user $ownerIdentity to recognize what user you're targetting;
-		//login page has to fall on this session as well
-		echo $ownerIdentity;
-	}
-	else
-	{
-		header('location: login.php');
-	}
+	if(isset($_SESSION['password']))
+	{session_unset($_SESSION['password']);}
+
+	
+	$ownerIdentity = $_SESSION['ownerIdentity'];
+	
+	echo $ownerIdentity;
+
+	$restaurant = new Restaurant();
+	$allRestaurants = $restaurant->getAllRestaurants();
+	$allRestaurants = $restaurant->search();
+
 	
 
  ?><!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-	</head>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		<form action="" method="post">
+			<label for="zoek_gemeente">Zoek op gemeente</label><br/>
+			<input type="input" name="zoek_gemeente" id="zoek_gemeente">
 
-
-	<body>
-		
-		<a href="logout.php">Log Out</a>
-
-	</body>
+		</form>
+	<ul>
+	<?php
+	
+	while ($alleRestauranten = $allRestaurants->fetch_assoc())
+		{
+			//if($restaurant->m_sCityRestaurant == $searchCity){
+			
+			echo "<a href='#'><li class='opmaak'><p>" . $alleRestauranten['restaurant_city'] . "</p></li></a>";
+			//}
+		}
+	
+	
+	?>
+	</ul>
+	</div>
+</body>
 </html>
