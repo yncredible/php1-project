c7f064ae8f803c2d5f8aa8f2346cf9fd70a811c6
Yannick Nijs <yannicknijs@yannickn.local>
2014-05-06 22:51:51 +0200

    bootstrapped and cleaned up myrestaurants overview page

--- myRestaurants.php
+++ myRestaurants.php
@@ -6,8 +6,7 @@
 		header("location: login.php");
 	}
 
-
-	var_dump($_SESSION);
+	// var_dump($_SESSION);
 
 	include_once('classes/Restaurant.class.php');
 	$restaurant = new Restaurant();
@@ -17,22 +16,61 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Document</title>
+		<title>OpenTable | MyRestaurants</title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 	<body>
-		<a href="addRestaurant.php">Add a new restaurant</a>
+		<div class="container">
 
-		<ul>
+		<div class="jumbotron">
+			<h2>Overzicht van mijn restaurants</h2>
+		</div>
+		
+		<nav>
+			<ul class="nav nav-pills">
+				<li><a href="index.php">Home</a></li>
+				<li class=""><a href="addRestaurant.php">Add a new restaurant</a></li>
+			</ul>
+		</nav>
+
+		
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Naam restaurant</th>
+					<th>Gemeente</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					foreach($ownersRestaurants as $Orestaurant){ ?>
+						
+						<tr>
+							<td><a href="managerestaurant.php?id=<?php echo $Orestaurant['restaurant_id'];?>"><?php echo $Orestaurant['restaurant_name']; ?></a></td>
+							<td><?php echo $Orestaurant['restaurant_city']; ?></td>
+						</tr>
+
+					<?php }
+				?>
+			</tbody>
+		</table>
+
+
+
+<!-- 		<ul>
 			<?php 
-				foreach ($ownersRestaurants as $Orestaurant) 
-				{
-					echo "<li>";
-						echo "<a href='managerestaurant.php?id=". $Orestaurant["restaurant_id"] ."'>" ." ". $Orestaurant['restaurant_name'] . "</a>";
-						echo $Orestaurant['restaurant_city'];
-					echo "</li>";
-				}
+				//foreach ($ownersRestaurants as $Orestaurant) 
+				//{
+				//	echo "<li>";
+				//		echo "<a href='managerestaurant.php?id=". $Orestaurant["restaurant_id"] ."'>" ." ". $Orestaurant['restaurant_name'] . "</a>";
+				//		echo $Orestaurant['restaurant_city'];
+				//	echo "</li>";
+				//}
 			 ?>
 		</ul>
+ -->
+		</div>
 	</body>
 </html>
