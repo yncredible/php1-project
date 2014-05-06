b5d1d944fa90d485358a531b1517b96b7ee429f1
Yannick Nijs <yannicknijs@yannickn.local>
2014-05-06 23:40:47 +0200

    table overview now loops in a table, bootstrapped and tweaked

--- managerestaurant.php
+++ managerestaurant.php
@@ -26,17 +26,30 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Document</title>
+		<title>OpenTable | Uw restaurant</title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 
 	<body>
-		<?php 
-			foreach ($ownersRestaurants as $restaurant) 
-			{
-				echo $restaurant['restaurant_name'];
-			}
-		 ?>
+		<div class="container">
+
+		<div class="jumbotron">
+			<?php 
+				foreach ($ownersRestaurants as $restaurant) { ?>
+					<h2>Manage <?php echo $restaurant['restaurant_name']; ?></h2>
+				<?php }
+			 ?>		
+		</div>
+
+		<nav>
+			<ul class="nav nav-pills">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="myRestaurants.php">My Restaurants</a></li>
+			</ul>
+		</nav>
+
 
 		
 		 <div id="menu">
@@ -78,24 +91,57 @@
 		 	 <a href="addMenu.php?id=<?php echo $restaurantID; ?>">Manage menu</a>
 		 </div>
 
-		<div id="tables">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Tables overview</h2>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Table nr.</th>
+							<th>Persons</th>
+							<th>Description</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+							foreach($tableList as $tableItem){ ?>
+									
+									<tr>
+										<td><?php echo $tableItem['table_nr']; ?></td>
+										<td><?php echo $tableItem['table_persons']; ?></td>
+										<td><?php echo $tableItem['table_description']; ?></td>
+										<td><?php echo $tableItem['table_status']; ?></td>
+									</tr>
+
+							<?php }
+						?>
+					</tbody>
+				</table>
+				
+				<p><a href="addTable.php?id=<?php echo $restaurantID; ?>">Manage tables</a></p>
+
+			</div>
+		</div> 
+
+<!-- 		<div id="tables">
 		 	<h1>Tables</h1>
 		 	<ul>
 				<?php 
-					foreach ($tableList as $tableItem) 
-					{
-						echo "<li>";
-							echo $tableItem['table_nr'];
-							echo $tableItem['table_persons'];
-							echo $tableItem['table_description'];
-							echo $tableItem['table_status'];
-						echo "</li>";
-					}
+					// foreach ($tableList as $tableItem) 
+					// {
+					// 	echo "<li>";
+					// 		echo $tableItem['table_nr'];
+					// 		echo $tableItem['table_persons'];
+					// 		echo $tableItem['table_description'];
+					// 		echo $tableItem['table_status'];
+					// 	echo "</li>";
+					// }
 				 ?>
 			</ul>
 		 	 <a href="addTable.php?id=<?php echo $restaurantID; ?>">Manage tables</a>
 		 </div>
-
-
+ -->
+		</div>
 	</body>
 </html>
