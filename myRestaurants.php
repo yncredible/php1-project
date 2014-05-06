1f05df22e6de8d93c81fa18ff100f4b1b2937a63
Kristof Van Espen <kristof@creativebrain.be>
2014-05-06 10:51:07 +0200

    Added list of owner's restaurants

---
+++ myRestaurants.php
@@ -0,0 +1,36 @@
+<?php 
+	session_start();
+
+	if(!isset($_SESSION['ownerIdentity']))
+	{
+		header("location: login.php");
+	}
+
+	include_once('classes/Restaurant.class.php');
+	$restaurant = new Restaurant();
+	$ownersRestaurants = $restaurant->getAllRestaurantsFromAnOwner($_SESSION['ownerIdentity']);
+
+
+ ?><!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+
+	<body>
+		<a href="addRestaurant.php">Add a new restaurant</a>
+
+		<ul>
+			<?php 
+				foreach ($ownersRestaurants as $Orestaurant) 
+				{
+					echo "<li>";
+						echo "<a href='managerestaurant.php?id=". $Orestaurant["restaurant_id"] ."'>" . $Orestaurant['restaurant_name'] . "</a>";
+						echo $Orestaurant['restaurant_city'];
+					echo "</li>";
+				}
+			 ?>
+		</ul>
+	</body>
+</html>
