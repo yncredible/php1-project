908a47b0440c6845edfdcc2dae8f3d5a09de3c11
Yannick Nijs <yannicknijs@yannickn.local>
2014-05-06 23:01:35 +0200

    tuned form labels

--- addRestaurant.php
+++ addRestaurant.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	var_dump($_SESSION);
+
 	if(!isset($_SESSION['ownerIdentity']))
  	{
  		header("location: login.php");
@@ -32,34 +32,63 @@
 			header("location: managerestaurant.php?id=$restaurantID");
 		}
 	}
-	var_dump($_SESSION);
+	// var_dump($_SESSION);
 
 ?><!doctype html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Add restaurants</title>
+		<title>OpenTable | Add restaurant</title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 
 	<body>
-		<form method="post">
-			<label>Name Restaurant</label>
-			<input type="text" name="name_restaurant" id="name_restaurant"><br/>
-			<label>Street </label>
-			<input type="text" name="street_restaurant" id="street_restaurant"><br/>
-			<label>Number</label>
-			<input type="text" name="number_restaurant" id="number_restaurant"><br/>
-			<label>Postalcode</label>
-			<input type="text" name="postalcode_restaurant" id="postalcode_restaurant"><br/>
-			<label>City</label>
-			<input type="text" name="city_restaurant" id="city_restaurant"><br/>
-			<label>Email</label>
-			<input type="email" name="email_restaurant" id="email_restaurant"><br/>
-			<label>Website</label>
-			<input type="text" name="website_restaurant" id="website_restaurant"><br/>
+		<div class="container">
+			<div class="jumbotron">
+				<h2>Add a new restaurant</h2>
+			</div>
+
+			<nav>
+				<ul class="nav nav-pills">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="myRestaurants.php">My Restaurants</a></li>
+				</ul>
+			</nav>
+			
+			<div class="row">
+				<div class="col-sm-12">
+
+				<form method="post" role="form">
+					<label for="name_restaurant">Name Restaurant</label>
+					<input type="text" name="name_restaurant" id="name_restaurant"><br/>
+					<label for="street_restaurant">Street </label>
+					<input type="text" name="street_restaurant" id="street_restaurant"><br/>
+					<label for="number_restaurant">Number</label>
+					<input type="text" name="number_restaurant" id="number_restaurant"><br/>
+					<label for="postalcode_restaurant">Postalcode</label>
+					<input type="text" name="postalcode_restaurant" id="postalcode_restaurant"><br/>
+					<label for="city_restaurant">City</label>
+					<input type="text" name="city_restaurant" id="city_restaurant"><br/>
+					<label for="email_restaurant">Email</label>
+					<input type="email" name="email_restaurant" id="email_restaurant"><br/>
+					<label for="website_restaurant">Website</label>
+					<input type="text" name="website_restaurant" id="website_restaurant"><br/>
 
 
-			<input type="submit" value="Add restaurant" name="register_restaurant" id="register_restaurant">
-		</form>
+					<input type="submit" value="Add restaurant" name="register_restaurant" id="register_restaurant">
+				</form>
+			
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+		</div>
 	</body>
 </html>
