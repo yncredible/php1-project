bad495bec58fdfa8719858bf7c3733e08511e1a5
Kristof Van Espen <kristof@creativebrain.be>
2014-05-06 16:16:48 +0200

    Added dynamic table link and loop for tables

--- managerestaurant.php
+++ managerestaurant.php
@@ -12,7 +12,14 @@
 	include_once('classes/Restaurant.class.php');
 	$restaurant = new Restaurant();
 	$ownersRestaurants = $restaurant->getSpecificRestaurantfromOwner($restaurantID);
+
+	include_once('classes/Menu.class.php');
+	$menu = new Menu();
+	$menuList = $menu->getSpecificMenufromRestaurant($restaurantID);
 	
+	include_once('classes/Table.class.php');
+	$table = new Table();
+	$tableList = $table->getSpecificTablesfromRestaurant($restaurantID);
 	//var_dump($ownersRestaurants);
 
  ?><!doctype html>
@@ -33,13 +40,60 @@
 
 		
 		 <div id="menu">
-		 	 <h1>Echo out Menu here too</h1>
+		 <h1>Menu</h1>
+		 	 <h2>Beverages</h2>
+			  <ul>
+			  	<?php 
+			  		foreach ($menuList as $menuItem) 
+			  		{
+			  			if($menuItem['menu_category'] == "beverages")
+			  			{
+			  				echo "<li>";
+			  					echo $menuItem['menu_name'];
+			  					echo $menuItem['menu_description'];
+			  					echo $menuItem['menu_price'];
+			  				echo "</li>";
+			  			}
+			  		}
+			  	 ?>
+			  </ul>
+
+
+			  <h2>Burger/Sandwiches</h2>
+			  <ul>
+			  	<?php 
+			  		foreach ($menuList as $menuItem) 
+			  		{
+			  			if($menuItem['menu_category'] == "burgerSandwiches")
+			  			{
+			  				echo "<li>";
+			  					echo $menuItem['menu_name'];
+			  					echo $menuItem['menu_description'];
+			  					echo $menuItem['menu_price'];
+			  				echo "</li>";
+			  			}
+			  		}
+			  	 ?>
+			  </ul>
 		 	 <a href="addMenu.php?id=<?php echo $restaurantID; ?>">Manage menu</a>
 		 </div>
 
 		<div id="tables">
-		 	 <h1>Echo out Tables here too</h1>
-		 	 <a href="addTable.php?id=" <?php echo $restaurantID; ?>>Manage tables</a>
+		 	<h1>Tables</h1>
+		 	<ul>
+				<?php 
+					foreach ($tableList as $tableItem) 
+					{
+						echo "<li>";
+							echo $tableItem['table_nr'];
+							echo $tableItem['table_persons'];
+							echo $tableItem['table_description'];
+							echo $tableItem['table_status'];
+						echo "</li>";
+					}
+				 ?>
+			</ul>
+		 	 <a href="addTable.php?id=<?php echo $restaurantID; ?>">Manage tables</a>
 		 </div>
 
 
