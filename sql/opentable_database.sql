e3ee90943c7d884cb59dae6d8b20a2f015279066
Kristof Van Espen <kristof@creativebrain.be>
2014-05-07 10:40:04 +0200

    Database update changes

--- sql/opentable_database.sql
+++ sql/opentable_database.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2014-05-05 21:02:43
+Date: 2014-05-07 10:35:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,12 +27,17 @@ CREATE TABLE `menu` (
   `menu_category` varchar(255) NOT NULL,
   `restaurant_id` int(11) NOT NULL,
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
 INSERT INTO `menu` VALUES ('1', 'Balletjes', 'met tomatensaus', '500', 'hoofdgerecht', '25');
+INSERT INTO `menu` VALUES ('2', 'Sloppy joe\'s', 'Big piece of diabetes', '4', 'burgerSandwiches', '6');
+INSERT INTO `menu` VALUES ('3', 'Balletjes', 'Big piece of diabetes', '500', 'kidsMenu', '3');
+INSERT INTO `menu` VALUES ('4', 'Sloppy joe\'s', 'Big piece of diabetes', '500', 'burgerSandwiches', '3');
+INSERT INTO `menu` VALUES ('5', 'Balletjes', 'met tomatensaus', '500', 'beverages', '13');
+INSERT INTO `menu` VALUES ('6', 'Sloppy joe\'s', 'Big piece of diabetes', '4', 'burgerSandwiches', '5');
 
 -- ----------------------------
 -- Table structure for `openinghours`
@@ -98,7 +103,7 @@ CREATE TABLE `restaurant` (
   `menu_id` int(11) NOT NULL,
   `openingHours_id` int(11) NOT NULL,
   PRIMARY KEY (`restaurant_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of restaurant
@@ -106,6 +111,25 @@ CREATE TABLE `restaurant` (
 INSERT INTO `restaurant` VALUES ('1', 'Segers', 'Em van der velde straat', '97', '2830', 'Willebroek', 'kimberlygs@hotmail.be', 'www.hallo.com', '0', '0', '0');
 INSERT INTO `restaurant` VALUES ('2', 'Segers', 'Em van der velde straat', '97', '2830', 'Willebroek', 'kimberlygs@hotmail.be', 'www.hallo.com', '0', '0', '0');
 INSERT INTO `restaurant` VALUES ('3', 'Pizza Hut', 'Markt', '3', '1840', 'Londerzeel', 'pizzahut@pizza.be', 'www.pizza.be', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('4', 'burger', 'burger', '32', '1743', 'burger', 'burger@hotmail.com', 'www.burger.be', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('5', 'Taco Bell', 'Chickens', '21', '4312', 'qebtrsgd', 'tacoballs@hotmail.com', 'www.tacoballs.be', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('6', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('7', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('8', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('9', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('10', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('11', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('12', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('13', 'Pizza Hut', 'burger', '21', '1743', 'Coin', 'ebsdg@erqbf.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('14', 'Big joe\'s', 'Fancy street', '14', '4212', 'Joe Street', 'bigjoe@gmail.com', 'www.bigjoe.com', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('15', 'Big joe\'s', 'Fancy street', '14', '4212', 'Joe Street', 'bigjoe@gmail.com', 'www.bigjoe.com', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('16', 'Big joe\'s', 'Fancy street', '3', '1743', 'Joe Street', 'burger@hotmail.com', 'www.bigjoe.com', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('17', 'Big joe\'s', 'Fancy street', '3', '1743', 'Joe Street', 'burger@hotmail.com', 'www.bigjoe.com', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('18', 'Big joe\'s', 'Fancy street', '3', '1743', 'Joe Street', 'burger@hotmail.com', 'www.bigjoe.com', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('19', 'Pizza Hut', 'Fancy street', '3', '4212', 'burger', 'pizzahut@pizza.be', 'www.tacoballs.be', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('20', 'Pizza Hut', 'Fancy street', '3', '4212', 'burger', 'pizzahut@pizza.be', 'www.tacoballs.be', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('21', 'New restaurant', 'Londerzeel', '13', '1840', 'Londerzeel', 'bigjoe@gmail.com', 'www.wbtsg.gerf', '25', '0', '0');
+INSERT INTO `restaurant` VALUES ('22', 'New test restaurant', 'testing ', '33', '4325', 'Testing City', 'testingrestaurant@yahoo.com', 'www.testingrestaurant.com', '25', '0', '0');
 
 -- ----------------------------
 -- Table structure for `restaurateur`
@@ -139,8 +163,10 @@ CREATE TABLE `table` (
   `table_description` longtext NOT NULL,
   `restaurant_id` int(11) NOT NULL,
   PRIMARY KEY (`table_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of table
 -- ----------------------------
+INSERT INTO `table` VALUES ('1', '7', '4', 'Occupied', 'A nice view out the window', '3');
+INSERT INTO `table` VALUES ('2', '7', '4', 'Occupied', 'A nice view out the window', '3');
