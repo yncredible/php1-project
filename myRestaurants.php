b51fc57ced49affa0806872a1dbb07cba2745eee
Jens Ivens <jens.ivens@hotmail.com>
2014-05-07 14:05:52 +0200

    Delete comment

--- myRestaurants.php
+++ myRestaurants.php
@@ -68,20 +68,6 @@
 			</tbody>
 		</table>
 
-
-
-<!-- 		<ul>
-			<?php 
-				//foreach ($ownersRestaurants as $Orestaurant) 
-				//{
-				//	echo "<li>";
-				//		echo "<a href='managerestaurant.php?id=". $Orestaurant["restaurant_id"] ."'>" ." ". $Orestaurant['restaurant_name'] . "</a>";
-				//		echo $Orestaurant['restaurant_city'];
-				//	echo "</li>";
-				//}
-			 ?>
-		</ul>
- -->
  		</div>
 	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
