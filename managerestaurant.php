a5fcb8287a83b49f415174ea3a053ab7709c6dc1
Jens Ivens <jens.ivens@hotmail.com>
2014-05-07 13:53:08 +0200

    Add reset.css

--- managerestaurant.php
+++ managerestaurant.php
@@ -54,6 +54,7 @@
 		<meta charset="UTF-8">
 		<title>OpenTable | Uw restaurant</title>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="css/reset.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style.css">
 
