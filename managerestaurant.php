ac704b7d043a5dae9c9fd517d7d42b23489ae067
Kristof Van Espen <kristof@creativebrain.be>
2014-05-07 11:04:10 +0200

    Merged file with addMenu.php and addTable.php

--- managerestaurant.php
+++ managerestaurant.php
@@ -9,18 +9,43 @@
 
 	$restaurantID = $_GET['id'];
 
+	include_once("classes/Menu.class.php");
+ 	$menu = new Menu();
+	if(!empty($_POST['add_menu']))
+	{
+		//add a new menu item
+		$menu->menuName = $_POST["menu_name"];
+		$menu->menuDescription = $_POST["menu_description"];
+		$menu->menuPrice = $_POST["menu_price"];
+		$menu->menuCategory = $_POST["menu_category"];
+
+		$menu->addMenu($restaurantID);
+	}
+
+	include_once('classes/Table.class.php');
+	$table = new Table();
+	if(!empty($_POST['table_add']))
+	{
+		//add a new table item
+		$table->Number = $_POST['table_number'];
+		$table->Persons = $_POST['table_people'];
+		$table->Status = $_POST['table_status'];
+		$table->Description = $_POST['table_description'];
+
+		$table->addTable($restaurantID);
+	}
+
+
+	//show owner restaurants
 	include_once('classes/Restaurant.class.php');
 	$restaurant = new Restaurant();
 	$ownersRestaurants = $restaurant->getSpecificRestaurantfromOwner($restaurantID);
 
-	include_once('classes/Menu.class.php');
-	$menu = new Menu();
+	//show restaurant menu
 	$menuList = $menu->getSpecificMenufromRestaurant($restaurantID);
 	
-	include_once('classes/Table.class.php');
-	$table = new Table();
+	//show restaurant tables
 	$tableList = $table->getSpecificTablesfromRestaurant($restaurantID);
-	//var_dump($ownersRestaurants);
 
  ?><!doctype html>
 <html lang="en">
@@ -88,8 +113,36 @@
 			  		}
 			  	 ?>
 			  </ul>
-		 	 <a href="addMenu.php?id=<?php echo $restaurantID; ?>">Manage menu</a>
 		 </div>
+
+		 <form action='#' method="post">
+		    <label for="menu_name">Name</label>
+		    <input id="menu_name" name="menu_name" />
+
+		    <label for="menu_description">Description</label>
+		    <input id="menu_description" name="menu_description" />
+
+		    <label for="menu_price">Price</label>
+		    <input id="menu_price" name="menu_price" />
+
+			<label for="menu_category">Category</label>
+			<select id="menu_category" name="menu_category">
+					<option value="beverages">Beverages</option>
+					<option value="alcoholBeverages">Alcoholic beverages</option>
+					<option value="appetizers">Appetizers</option>
+					<option value="soups">Soups</option>
+					<option value="salads">Salads</option>
+					<option value="chicken">Chicken</option>
+					<option value="pasta">Pasta</option>
+					<option value="seafood">Seafood</option>
+					<option value="ribSteaks">Rib/Steaks</option>
+					<option value="burgerSandwiches">Burger/Sandwiches</option>
+					<option value="kidsMenu">Kids Menu</option>
+					<option value="desserts">Desserts</option>
+			</select>
+
+	 		<button type="submit" id='btnSubmit' name="add_menu">Add!</button>
+ 		</form>
 
 		<div class="row">
 			<div class="col-sm-12">
@@ -119,29 +172,79 @@
 					</tbody>
 				</table>
 				
-				<p><a href="addTable.php?id=<?php echo $restaurantID; ?>">Manage tables</a></p>
-
 			</div>
 		</div> 
 
-<!-- 		<div id="tables">
-		 	<h1>Tables</h1>
-		 	<ul>
-				<?php 
-					// foreach ($tableList as $tableItem) 
-					// {
-					// 	echo "<li>";
-					// 		echo $tableItem['table_nr'];
-					// 		echo $tableItem['table_persons'];
-					// 		echo $tableItem['table_description'];
-					// 		echo $tableItem['table_status'];
-					// 	echo "</li>";
-					// }
-				 ?>
-			</ul>
-		 	 <a href="addTable.php?id=<?php echo $restaurantID; ?>">Manage tables</a>
-		 </div>
- -->
+		<div class="row">
+		<div class="col-sm-12">
+
+		<form action="" method="POST" role="form" class="form-inline">
+			
+			<div class="form-group">
+			<label for="tableNumber">Table number</label>
+			<select class="form-control" id="tableNumber" name="table_number">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+				<option value="13">13</option>
+				<option value="14">14</option>
+				<option value="15">15</option>
+				<option value="16">16</option>
+				<option value="17">17</option>
+				<option value="18">18</option>
+				<option value="19">19</option>
+				<option value="20">20</option>
+			</select>
+			</div>
+
+			<div class="form-group">
+			<label for="tablePeople">Amount of people</label>
+			<select class="form-control" id="tablePeople" name="table_people">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+			</select>
+			</div>
+
+			<div class="form-group">
+			<label for="tableDescription">Description</label>
+   			<input class="form-control" id="tableDescription" name="table_description"/>
+			</div>
+   			
+			<div class="form-group">
+   			<label for="tableStatus">Status</label>
+			<select class="form-control" id="tableStatus" name="table_status">
+				<option value="Free">Free</option>
+				<option value="Reserved">Reserved</option>
+				<option value="Occupied">Occupied</option>
+			</select>
+			</div>
+			
+			<div class="form-group">
+			<input class="btn btn-primary" type="submit" name="table_add" value="Add table">
+			</div>
+
+		</form>
+		</div>
+		</div>
+
+
 		</div>
 	</body>
 </html>
