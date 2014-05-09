46dec0c2057d969c0cf88bf2b3ac203239438b13
Kristof Van Espen <kristof@creativebrain.be>
2014-05-09 19:45:23 +0200

    Added Specific get arrays and Delete functions

--- classes/Menu.class.php
+++ classes/Menu.class.php
@@ -69,13 +69,30 @@
 									". $restaurantID ."
 								)";
 
-			$db->conn->query($sql);
+			$result = $db->conn->query($sql);
+
+			if($result)
+			{
+				$sql2 = "SELECT LAST_INSERT_ID() FROM menu order by menu_id desc limit 1";
+				$result2 = $db->conn->query($sql2);
+
+				if($result2)
+				{
+					$results = mysqli_fetch_array($result2, MYSQL_ASSOC);
+
+					$lastEntryID = $results['LAST_INSERT_ID()'];
+
+					json_encode($lastEntryID);
+					echo $lastEntryID;
+				}
+			}
 		}
 
-		public function getSpecificMenufromRestaurant($restaurantID)
+
+		public function getBeverages($restaurantID)
 		{
 			$db = new Db();
-			$sql = "SELECT * FROM menu WHERE restaurant_id = '$restaurantID'";
+			$sql = "SELECT * FROM menu WHERE menu_category = 'beverages' AND restaurant_id = '$restaurantID'";
 			
 			$result = $db->conn->query($sql);
 			if($result)
@@ -87,6 +104,203 @@
 				}
 				return $rows;
 			}
+		}
+
+		public function getAlcoholBeverages($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'alcoholBeverages' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getAppetizers($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'appetizers' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getSoups($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'soups' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getSalads($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'salads' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getChicken($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'chicken' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getPasta($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'pasta' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getSeafood($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'seafood' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getRibSteaks($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'ribSteaks' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getBurgerSandwiches($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'burgerSandwiches' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getKidsMenu($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'kidsMenu' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		public function getDesserts($restaurantID)
+		{
+			$db = new Db();
+			$sql = "SELECT * FROM menu WHERE menu_category = 'desserts' AND restaurant_id = '$restaurantID'";
+			
+			$result = $db->conn->query($sql);
+			if($result)
+			{
+				$rows = array();
+				while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+				{
+				    $rows[] = $row;
+				}
+				return $rows;
+			}
+		}
+
+		function DeleteMenuItem($MenuItemID)
+		{
+			$db = new Db();
+
+			$sql = "DELETE FROM menu
+					WHERE menu_id = '$MenuItemID'";
+
+			$result = $db->conn->query($sql);
 		}
 	}
 ?>
